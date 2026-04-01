76bccbddb9b887d0ffdfff08bd3ac856eb12d5c4
blue <blue@b.com>
2026-04-01 15:52:10 +0300

    r6

--- K.java
+++ K.java
@@ -11,12 +11,4 @@ public class K {
     public long dd() {
         return 99999;
     }
-
-    public java.util.Random mm() {
-        return new java.util.Random();
-    }
-
-    public java.lang.Class qq() {
-        return getClass();
-    }
 }
